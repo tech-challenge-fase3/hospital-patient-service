9a973547e63c522e1706193f844e24c81344081e
Eber Lasso <eber.slasso@gmail.com>
2026-09-05 15:43:29 -0300

    chore: initial project structure with Spring Boot and dependencies for patient service

---
+++ src/main/java/com/hospital/hospital_patient_service/HospitalPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_patient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/hospital_patient_service/HospitalPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
